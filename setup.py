b246f20c9ef7110195fd26f4e87c90798acb1b3a
Aaron David Schneider <aaron.schneider@nbi.ku.dk>
2022-02-09 15:48:49 +0100

    initial commit

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='GCMtools',
+    version='v0.1',
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    url='https://github.com/exorad/GCMtools',
+    license='MIT',
+    author='Aaron David Schneider',
+    author_email='aarondavid.schneider@nbi.ku.dk',
+    description='postprocessing stuff',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    install_requires=[
+        "scipy>=1.7.0",
+        "numpy",
+    ]
+)
