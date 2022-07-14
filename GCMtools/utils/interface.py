086f7ddbbfa0b20927a375ce0a2a5a2a426db7a1
Aaron David Schneider <aaron.schneider@nbi.ku.dk>
2022-07-14 10:54:50 +0200

    phase curve working and tested. Documentation will follow

--- GCMtools/utils/interface.py
+++ GCMtools/utils/interface.py
@@ -12,237 +12,8 @@
 
 import numpy as np
 import xarray as xr
+
 from ..core.const import VARNAMES as c
-
-
-class Interface:
-    """
-    The GCMtools interfacing class which implements common
-    interfacing functionalities to different codes.
-
-    Methods
-    -------
-    _regrid: regrids data to different resolution
-
-    """
-    def _regrid(self, data, dlon, dlat):
-        """
-        Regrid data to a different resolution
-
-        Parameters
-        ----------
-        data: xr.DataSet or xr.DataArray
-            The data that should be regridded
-        dlon: float
-            the steps in longitutde (longitude resolution)
-        dlat: float
-            the steps in latititude (latitude resolution)
-
-        Returns
-        -------
-        data_interp: xr.DataSet or xr.DataArray
-            The data that has been regridded
-        """
-        lonb = np.arange(-180,180+dlon,dlon)
-        latb = np.arange(-90, 90+dlat, dlat)
-
-        lons = 0.5 * (lonb[1:] + lonb[:-1])
-        lats = 0.5 * (latb[1:] + latb[:-1])
-
-        return data.interp(lon=lons, lat=lats)
-
-
-class PACInterface(Interface):
-    def to_2D_PAC(self, ds):
-        """
-        Transform the given GCM dataset to a pseudo-2D PAC
-        input file.
-
-        Parameters
-        ----------
-        ds: DataSet
-            A GCMtools-compatible dataset of a 3D climate simulation.
-
-        """
-        pass
-
-
-class pRTInterface(Interface):
-    """
-    Interface with petitRADTRANS
-    """
-
-    def __init__(self, pRT):
-        """
-        Constructs the Interface and links to pRT.
-
-        Parameters
-        ----------
-        pRT: petitRADTRANS.Radtrans
-            A pRT Radtrans object
-        """
-        self.chemistry = Chemistry()
-        self.pRT = pRT
-
-    def set_data(self, ds, regrid_lowres_phase=False):
-        """
-        Set the data to be used for the interface
-
-        Parameters
-        ----------
-        ds: DataSet
-            A GCMtools-compatible dataset of a 3D climate simulation.
-            Should not include a time coordinate!
-        regrid_lowres_phase: bool, optional
-            regrid the dataset to a 15deg resolution.
-            Needed for Phasecurve
-        """
-        if regrid_lowres_phase:
-            ds = self._regrid(data=ds, dlon=15, dlat=15)
-
-        self.ds = ds
-        self.chemistry.set_data(ds)
-
-    def chem_from_poorman(self, temp_key='T', CO=0.55, FeH=0.0):
-        """
-        Calculate equilibrium abundancies with poorman from pRT
-
-        Parameters
-        ----------
-        temp_key: str, optional
-            The key to the temperature field used for the abundancies.
-            Defaults to T.
-        CO: float, optional
-            The C/O ratio. Currently only one global value allowed. Defaults to 0.55.
-        FeH: float, optional
-            The metalicity ratio. Currently only one global value allowed. Defaults to 0.0.
-        """
-        return self.chemistry.chem_from_poorman(temp_key=temp_key, CO=CO, FeH=FeH)
-
-    def calc_phase_spectrum(self, mmw, filename=None, **prt_args):
-        """
-        Calculate the spectrum for a phasecurve.
-
-        Parameters
-        ----------
-        mmw: float
-            Mean molecular weight (in atomic units).
-        filename: str
-            path at which the output should be stored.
-        prt_args:
-            All the args that should be parsed to calc_spectra.
-            See the docs of prt_phasecurve for more info on the arguments.
-            Needed args:
-                Rstar, gravity
-
-        Returns
-        -------
-        spectra: xr.DataArray
-            Dataarray containing the spectrum. The spectrum is normed to the stellar spectrum.
-
-        """
-        from prt_phasecurve import calc_spectra
-        import petitRADTRANS.nat_cst as nc
-
-        abus = self.chemistry.to_pRT(self.pRT.line_species, self.pRT.press/1e6)
-        mu = np.cos(abus[c['lon']] * np.pi / 180.) * np.cos(abus[c['lat']] * np.pi / 180.)
-        theta_star = np.arccos(mu) * 180 / np.pi
-        pRT_abu_keys = list(set(abus.keys()) - {'T', 'nabla_ad', 'MMW'})
-
-        lon, lat = np.meshgrid(abus[c['lon']], abus[c['lat']])
-
-        theta_list, temp_list, abunds_list = [], [], []
-        for i, lon_i in enumerate(np.array(lon).flat):
-            lat_i = np.array(lat).flat[i]
-            theta_list.append(theta_star.sel(lon=lon_i, lat=lat_i).values)
-            temp_list.append(abus[c['T']].sel(lon=lon_i, lat=lat_i).values)
-            abunds_list.append({sp: abus[sp].sel(lon=lon_i, lat=lat_i).values for sp in pRT_abu_keys})
-
-        gravity_cgs = prt_args.pop('gravity', self.ds.attrs.get(c['g'])*100)
-        wlen = nc.c / self.pRT.freq / 1e-4
-        stellar_spectrum = self._get_stellar_spec(wlen=wlen, **prt_args)
-
-        spectra_raw = calc_spectra(self.pRT, temp=temp_list, gravity=gravity_cgs, mmw=mmw, abunds=abunds_list,
-                                    theta_star=theta_list, **prt_args)
-
-        R_star = prt_args.get('Rstar')
-        if R_star is None:
-            raise ValueError('pRT needs Rstar, please set Rstar to the stellar radius in cm.')
-
-        R_p = self.ds.attrs.get(c['R_p'])
-        if R_p is None:
-            raise ValueError(f'pRT needs the planetary radius [in m]. Please use this function only with a GCMT processed dataset.')
-
-        spectra_raw = np.array(spectra_raw) * ((R_p*100)/(R_star))**2
-        spectra_raw = spectra_raw/stellar_spectrum[np.newaxis, np.newaxis, :]
-
-        Nmus = spectra_raw.shape[1]
-        spectra = xr.DataArray(
-            data=np.empty((len(abus[c['lon']]), len(abus[c['lat']]), Nmus, len(wlen))),
-            dims=[c['lon'], c['lat'], 'imu', 'wlen'],
-            coords={c['lon']: abus[c['lon']],
-                    c['lat']: abus[c['lat']],
-                    'imu': range(Nmus),
-                    'wlen': wlen})
-
-        for i, lon_i in enumerate(np.array(lon).flat):
-            lat_i = np.array(lat).flat[i]
-            spectra.loc[{c['lon']: lon_i, c['lat']: lat_i}] = spectra_raw[i]
-
-        if filename is not None:
-            spectra.to_netcdf(filename)
-
-        return spectra
-
-    def phase_curve(self, phases, spectra=None, filename=None):
-        """
-        Do the diskintegration of the spectrum to yield the phasecurve
-
-        Parameters
-        ----------
-        phases (array(P)):
-            List of phases at which the phasecurve should be evaluated
-        spectra: str
-            The spectrum generated by calc_phase_spectrum
-        filename: str
-            Alternatively give a filename where the spectrum is saved
-
-        Returns
-        -------
-        phasecurve: xr.DataArray
-            DataArray containing the phasecurveinformation
-
-        """
-        from prt_phasecurve import phase_curve
-
-        if spectra is None and filename is None:
-            raise ValueError('please provide a file with the spectrum or a spectrum produced by calc_phase_spectrum')
-
-        if filename is not None and spectra is None:
-            spectra = xr.open_dataarray(filename)
-
-        lon, lat = np.meshgrid(spectra[c['lon']], spectra[c['lat']])
-
-        ph_c = phase_curve(phases=phases, intensity=spectra, lon=lon, lat=lat)
-
-        return xr.DataArray(
-            data=ph_c,
-            dims=['phase', 'wlen'],
-            coords={'phase': phases,
-                    'wlen': spectra.wlen})
-
-    def _get_stellar_spec(self, wlen, Tstar, **kwargs):
-        """
-        Helperfunction from petitRADTRANS that calculates the stellar spectrum from the phoenix spectrum
-        """
-        from petitRADTRANS.nat_cst import get_PHOENIX_spec_rad
-        if Tstar != None:
-            spec, _ = get_PHOENIX_spec_rad(Tstar)
-            stellar_intensity = np.interp(wlen*1e-4, spec[:,0], spec[:,1])
-            # stellar_intensity = stellar_intensity * (Rstar / semimajoraxis) ** 2
-
-            return stellar_intensity
-
 
 
 class Chemistry:
@@ -297,7 +68,7 @@ class Chemistry:
         for key in [c['T'], *var_names]:
             self.abunds.update({key: xr.DataArray(
                 data=np.empty((len(self.ds[c['lon']]), len(self.ds[c['lat']]), len(self.ds[c['Z']]))),
-                dims=[c['lon'],c['lat'],c['Z']],
+                dims=[c['lon'], c['lat'], c['Z']],
                 coords={c['lon']: self.ds[c['lon']],
                         c['lat']: self.ds[c['lat']],
                         c['Z']: self.ds[c['Z']]})})
@@ -307,10 +78,10 @@ class Chemistry:
                 temp_i = self.ds[temp_key].sel(lon=lon, lat=lat)
                 abus = interpol_abundances(COs, FeHs, temp_i, p)
                 for sp in var_names:
-                    self.abunds[sp].loc[{c['lon']:lon, c['lat']:lat}] = abus[sp]
-                self.abunds[c['T']].loc[{c['lon']:lon, c['lat']:lat}] = temp_i
+                    self.abunds[sp].loc[{c['lon']: lon, c['lat']: lat}] = abus[sp]
+                self.abunds[c['T']].loc[{c['lon']: lon, c['lat']: lat}] = temp_i
 
-        self.abunds.attrs.update({'p_unit':p_unit})
+        self.abunds.attrs.update({'p_unit': p_unit})
 
     def to_pRT(self, prt_species, p_prt):
         """
@@ -321,7 +92,7 @@ class Chemistry:
         prt_species: list
             List of species names used in petitRADTRANS
         p_prt: list
-            List of species names used in petitRADTRANS
+            Pressure in bar, as used in petitRADTRANS
 
         Returns
         -------
@@ -341,8 +112,239 @@ class Chemistry:
             raise NotImplementedError('can currently only deal with Pa or bar')
 
         if p_unit == 'Pa':
-            p_prt = p_prt*1e5
+            p_prt = p_prt * 1e5
 
         prt_abu = prt_abu.interp(Z=p_prt)
 
         return prt_abu
+
+
+class Interface:
+    """
+    The GCMtools interfacing class which implements common
+    interfacing functionalities to different codes.
+
+    Methods
+    -------
+    set_data: Function that handles the input from GCMT
+    """
+
+    chemistry = Chemistry()
+
+    def set_data(self, ds, regrid_lowres = False):
+        """
+        Set the data to be used for the interface
+
+        Parameters
+        ----------
+        ds: DataSet
+            A GCMtools-compatible dataset of a 3D climate simulation.
+            Should not include a time coordinate!
+        regrid_lowres: bool, optional
+            Can be useful, if your GCMT uses a very detailed grid
+        """
+        if regrid_lowres:
+            dlon = 15
+            dlat = 15
+
+            lonb = np.arange(-180, 180 + dlon, dlon)
+            latb = np.arange(-90, 90 + dlat, dlat)
+
+            lons = 0.5 * (lonb[1:] + lonb[:-1])
+            lats = 0.5 * (latb[1:] + latb[:-1])
+
+            ds = ds.interp(lon=lons, lat=lats)
+
+        self.ds = ds
+        self.chemistry.set_data(ds)
+
+    def chem_from_poorman(self, temp_key='T', CO=0.55, FeH=0.0):
+        """
+        Calculate equilibrium abundancies with poorman from pRT
+
+        Parameters
+        ----------
+        temp_key: str, optional
+            The key to the temperature field used for the abundancies.
+            Defaults to T.
+        CO: float, optional
+            The C/O ratio. Currently only one global value allowed. Defaults to 0.55.
+        FeH: float, optional
+            The metalicity ratio. Currently only one global value allowed. Defaults to 0.0.
+        """
+        return self.chemistry.chem_from_poorman(temp_key=temp_key, CO=CO, FeH=FeH)
+
+
+
+class PACInterface(Interface):
+    def to_2D_PAC(self, ds):
+        """
+        Transform the given GCM dataset to a pseudo-2D PAC
+        input file.
+
+        Parameters
+        ----------
+        ds: DataSet
+            A GCMtools-compatible dataset of a 3D climate simulation.
+
+        """
+        pass
+
+
+class pRTInterface(Interface):
+    """
+    Interface with petitRADTRANS
+    """
+
+    def __init__(self, pRT):
+        """
+        Constructs the Interface and links to pRT.
+
+        Parameters
+        ----------
+        pRT: petitRADTRANS.Radtrans
+            A pRT Radtrans object
+        """
+        self.pRT = pRT
+
+    def calc_phase_spectrum(self, mmw, filename=None, normalize=True, **prt_args):
+        """
+        Calculate the spectrum for a phasecurve.
+
+        Parameters
+        ----------
+        mmw: float or 1D-array
+            Mean molecular weight (in atomic units). Will be globally uniform if float or horizonatally uniform if 1D.
+        filename: str
+            path at which the output should be stored.
+        normalize: bool
+            Decide if you want the spectrum to be normalized to the star. The code will then:
+            1. correct intensity for the ratio between solar radius and planetary radius
+            2. devide by stellar spectrum
+        prt_args:
+            All the args that should be parsed to calc_spectra.
+            See the docs of prt_phasecurve for more info on the arguments.
+            Needed args:
+                Rstar, gravity
+
+        Returns
+        -------
+        spectra: xr.DataArray
+            Dataarray containing the spectrum. The spectrum is normed to the stellar spectrum.
+
+        """
+        from prt_phasecurve import calc_spectra
+        import petitRADTRANS.nat_cst as nc
+
+        abus = self.chemistry.to_pRT(self.pRT.line_species, self.pRT.press / 1e6)
+        mu = np.cos(abus[c['lon']] * np.pi / 180.) * np.cos(abus[c['lat']] * np.pi / 180.)
+        theta_star = np.arccos(mu) * 180 / np.pi
+        pRT_abu_keys = list(set(abus.keys()) - {c['T'], 'nabla_ad', 'MMW'})
+
+        lon, lat = np.meshgrid(abus[c['lon']], abus[c['lat']])
+
+        if (lon.shape[0]*lon.shape[1]) > 300:
+            print('WARNING: Calculating a phasecurve on a fine grid takes very long. A resolution of 15 degrees is usually sufficient.')
+
+        theta_list, temp_list, abunds_list = [], [], []
+        for i, lon_i in enumerate(np.array(lon).flat):
+            lat_i = np.array(lat).flat[i]
+            theta_list.append(theta_star.sel(lon=lon_i, lat=lat_i).values)
+            temp_list.append(abus[c['T']].sel(lon=lon_i, lat=lat_i).values)
+            abunds_list.append({sp: abus[sp].sel(lon=lon_i, lat=lat_i).values for sp in pRT_abu_keys})
+
+        gravity_cgs = prt_args.pop('gravity', self.ds.attrs.get(c['g']) * 100)
+        wlen = nc.c / self.pRT.freq / 1e-4
+        stellar_spectrum = self._get_stellar_spec(wlen=wlen, **prt_args)
+        mmw = np.ones_like(self.pRT.press) * mmw  # broadcast if needed
+
+        spectra_raw = calc_spectra(self.pRT, temp=temp_list, gravity=gravity_cgs, mmw=mmw, abunds=abunds_list,
+                                   theta_star=theta_list, **prt_args)
+
+        R_star = prt_args.get('Rstar')
+        if R_star is None:
+            raise ValueError('pRT needs Rstar, please set Rstar to the stellar radius in cm.')
+
+        R_p = self.ds.attrs.get(c['R_p'])
+        if R_p is None:
+            raise ValueError(
+                f'pRT needs the planetary radius [in m]. Please use this function only with a GCMT processed dataset.')
+
+        if normalize:
+            # correct by (R_p/R_s)**2 and norm to stellar spectrum:
+            spectra_raw = np.array(spectra_raw) * ((R_p * 100) / (R_star)) ** 2
+            spectra_raw = spectra_raw / stellar_spectrum[np.newaxis, np.newaxis, :]
+
+        Nmus = spectra_raw.shape[1]
+        spectra = xr.DataArray(
+            data=np.empty((len(abus[c['lon']]), len(abus[c['lat']]), Nmus, len(wlen))),
+            dims=[c['lon'], c['lat'], 'imu', 'wlen'],
+            coords={c['lon']: abus[c['lon']],
+                    c['lat']: abus[c['lat']],
+                    'imu': range(Nmus),
+                    'wlen': wlen})
+
+        for i, lon_i in enumerate(np.array(lon).flat):
+            lat_i = np.array(lat).flat[i]
+            spectra.loc[{c['lon']: lon_i, c['lat']: lat_i}] = spectra_raw[i]
+
+        if filename is not None:
+            spectra.to_netcdf(filename)
+
+        return spectra
+
+    def phase_curve(self, phases, spectra=None, filename=None):
+        """
+        Do the diskintegration of the spectrum to yield the phasecurve
+
+        Parameters
+        ----------
+        phases (array(P)):
+            List of phases at which the phasecurve should be evaluated
+        spectra: str
+            The spectrum generated by calc_phase_spectrum
+        filename: str
+            Alternatively give a filename where the spectrum is saved
+
+        Returns
+        -------
+        phasecurve: xr.DataArray
+            DataArray containing the phasecurveinformation
+
+        """
+        from prt_phasecurve import phase_curve
+
+        if spectra is None and filename is None:
+            raise ValueError('please provide a file with the spectrum or a spectrum produced by calc_phase_spectrum')
+
+        if filename is not None and spectra is None:
+            spectra = xr.open_dataarray(filename)
+
+        lon, lat = np.meshgrid(spectra[c['lon']], spectra[c['lat']])
+
+        intensity, lat_list, lon_list = [], [], []
+        for i, lon_i in enumerate(np.array(lon).flat):
+            lat_i = np.array(lat).flat[i]
+            lat_list.append(lat_i)
+            lon_list.append(lon_i)
+            intensity.append(spectra.sel(lon=lon_i, lat=lat_i).values)
+
+        ph_c = phase_curve(phases=phases, intensity=intensity, lon=lon_list, lat=lat_list)
+
+        return xr.DataArray(
+            data=ph_c,
+            dims=['phase', 'wlen'],
+            coords={'phase': phases,
+                    'wlen': spectra.wlen})
+
+    def _get_stellar_spec(self, wlen, Tstar, **kwargs):
+        """
+        Helperfunction from petitRADTRANS that calculates the stellar spectrum from the phoenix spectrum
+        """
+        from petitRADTRANS.nat_cst import get_PHOENIX_spec_rad
+        if Tstar != None:
+            spec, _ = get_PHOENIX_spec_rad(Tstar)
+            stellar_intensity = np.interp(wlen * 1e-4, spec[:, 0], spec[:, 1])
+            # stellar_intensity = stellar_intensity * (Rstar / semimajoraxis) ** 2
+
+            return stellar_intensity
