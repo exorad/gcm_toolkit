5b993ef58f01fd8180aca6e6f5f8235474fb804c
Aaron David Schneider <aaron.schneider@nbi.ku.dk>
2022-07-27 14:55:02 +0200

    add flake8 and black precommit.

--- setup.py
+++ setup.py
@@ -2,31 +2,32 @@
 from setuptools import setup, find_packages
 
 
-with open("README.md", "r", encoding='utf-8') as fh:
+with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
-SETUP_REQUIRES = ['pytest-runner']
-TESTS_REQUIRE = ['pytest >= 4.0', 'coverage']
+SETUP_REQUIRES = ["pytest-runner"]
+TESTS_REQUIRE = ["pytest >= 4.0", "coverage"]
 INSTALL_REQUIRES = [
-        "scipy>=1.7.0",
-        "numpy",
-        "f90nml",
-        "astropy",
-        "xarray",
-        "pyyaml"
-    ]
+    "scipy>=1.7.0",
+    "numpy",
+    "f90nml",
+    "astropy",
+    "xarray",
+    "pyyaml",
+    "pre-commit",
+]
 
 setup(
-    name='gcmt',
-    version='v0.1.4',
+    name="gcmt",
+    version="v0.1.4",
     packages=find_packages(),
     include_package_data=True,
-    scripts=['bin/convert_to_gcmt'],
-    url='https://github.com/exorad/gcmt',
-    license='MIT',
-    author='Aaron David Schneider',
-    author_email='aarondavid.schneider@nbi.ku.dk',
-    description='postprocessing stuff',
+    scripts=["bin/convert_to_gcmt"],
+    url="https://github.com/exorad/gcmt",
+    license="MIT",
+    author="Aaron David Schneider",
+    author_email="aarondavid.schneider@nbi.ku.dk",
+    description="postprocessing stuff",
     long_description=long_description,
     long_description_content_type="text/markdown",
     install_requires=INSTALL_REQUIRES,
