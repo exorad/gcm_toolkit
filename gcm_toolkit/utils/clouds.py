2339f7b1c8d21b043747822f37ad5e1e3d8a8175
kiefersv <reronex@gmail.com>
2023-03-23 14:06:53 +0100

    minor corrections

--- gcm_toolkit/utils/clouds.py
+++ gcm_toolkit/utils/clouds.py
@@ -84,7 +84,7 @@ def cloud_opacities(wavelengths, cloud_radius, cloud_abundances, cloud_particle_
         for hig in range(len_h):
             # prepare input parameters for mie calculation
             m_eff = complex(eff[wav, hig, 0], -eff[wav, hig, 1])
-            x_fac = 2*np.pi*cloud_radius[hig]/wavelengths[wav]
+            x_fac = 2 * np.pi * cloud_radius[hig] / wavelengths[wav]
 
             # calculate mie theory
             qe_temp, qs_temp, _, _ = miepython.mie(m_eff, x_fac)
@@ -94,7 +94,7 @@ def cloud_opacities(wavelengths, cloud_radius, cloud_abundances, cloud_particle_
             qsca[hig, wav] = qs_temp
 
     # calculate cloud particle cross section
-    c_sec = cloud_abundances * 3/4 / (cloud_radius * cloud_particle_density)
+    c_sec = cloud_abundances * 3 / 4 / (cloud_radius * cloud_particle_density)
 
     # all done
     return qabs, qsca, c_sec
@@ -162,11 +162,11 @@ def eff_func(volume_fraction, wavelength, use_bruggemann=False):
                 val = complex(0, 0)
                 for k, key in enumerate(volume_fraction):
                     val += work[k, hig, wav, 0] * complex(work[k, hig, wav, 1],
-                                                          work[k, hig, wav, 2])**(1/3)
-                val = val**3
-                tmp = np.sqrt(val.real**2 + val.imag**2)
-                eff[wav, hig, 0] = np.sqrt(0.5*(val.real + tmp))
-                eff[wav, hig, 1] = np.sqrt(0.5*(-val.real + tmp)) * val.imag/np.abs(val.imag)
+                                                          work[k, hig, wav, 2]) ** (1 / 3)
+                val = val ** 3
+                tmp = np.sqrt(val.real ** 2 + val.imag ** 2)
+                eff[wav, hig, 0] = np.sqrt(0.5 * (val.real + tmp))
+                eff[wav, hig, 1] = np.sqrt(0.5 * (-val.real + tmp)) * val.imag / np.abs(val.imag)
 
     return eff
 
@@ -193,17 +193,17 @@ def _func(eff, work):
     val = np.zeros((2,))
     for i, _ in enumerate(work):
         # transform from n,k to e describtion (a,c = real part, b,d = imaginary part)
-        ava = work[i, 1]**2 - work[i, 2]**2
-        bva = 2*work[i, 1]*work[i, 2]
+        ava = work[i, 1] ** 2 - work[i, 2] ** 2
+        bva = 2 * work[i, 1] * work[i, 2]
         cva = eff[0]
         dva = eff[1]
 
         # calcualte vol_frac*(e - eff)/(e + 2*eff) for complex numbers
-        temp = ava**2 + 4*ava*cva + bva**2 + 4*bva*dva + 4*(cva**2 + dva**2)
-        val[0] += work[i, 0] * (ava**2 + ava*cva + bva**2 + bva*dva - 2*(cva**2 + dva**2)) / temp
-        val[1] += work[i, 0] * (3*bva*cva - 3*ava*dva)/temp
+        temp = ava ** 2 + 4 * ava * cva + bva ** 2 + 4 * bva * dva + 4 * (cva ** 2 + dva ** 2)
+        val[0] += work[i, 0] * (ava ** 2 + ava * cva + bva ** 2 + bva * dva - 2 * (cva ** 2 + dva ** 2)) / temp
+        val[1] += work[i, 0] * (3 * bva * cva - 3 * ava * dva) / temp
 
-    return val[0]**2 + val[1]**2
+    return val[0] ** 2 + val[1] ** 2
 
 
 def _cloud_nk_data(species, wavelength):
@@ -258,19 +258,19 @@ def _cloud_nk_data(species, wavelength):
                 for dnr, _ in enumerate(data):
                     cur_wave = float(data[dnr].split()[0])  # current wavelength
                     if wave < cur_wave:
-                        nlo = float(data[dnr-1].split()[1])  # lower n value
+                        nlo = float(data[dnr - 1].split()[1])  # lower n value
                         nhi = float(data[dnr].split()[1])  # higher n value
-                        klo = float(data[dnr-1].split()[2])  # lower k value
+                        klo = float(data[dnr - 1].split()[2])  # lower k value
                         khi = float(data[dnr].split()[2])  # higher k value
-                        prev_wave = float(data[dnr-1].split()[0])  # previous wavelength
+                        prev_wave = float(data[dnr - 1].split()[0])  # previous wavelength
 
                         # calculate interpolation
-                        fac = np.log(wave/prev_wave)/np.log(cur_wave/prev_wave)
-                        ref_index[wav, 0] = np.exp(np.log(nlo) + fac * np.log(nhi/nlo))
+                        fac = np.log(wave / prev_wave) / np.log(cur_wave / prev_wave)
+                        ref_index[wav, 0] = np.exp(np.log(nlo) + fac * np.log(nhi / nlo))
                         if klo <= 0 or khi < 0:
                             ref_index[wav, 1] = 0
                         else:
-                            ref_index[wav, 1] = np.exp(np.log(klo) + fac * np.log(khi/klo))
+                            ref_index[wav, 1] = np.exp(np.log(klo) + fac * np.log(khi / klo))
                         break
                 else:
                     # if wavelength is outside of range, extrapolate
@@ -291,17 +291,17 @@ def _cloud_nk_data(species, wavelength):
                         khi = float(data[-1].split()[2])  # higher k value
 
                         # calculate interpolation
-                        fac = np.log(wave/max_wave)/np.log(cur_wave/max_wave)
-                        ref_index[wav, 0] = np.exp(np.log(nhi) + fac * np.log(nlo/nhi))
+                        fac = np.log(wave / max_wave) / np.log(cur_wave / max_wave)
+                        ref_index[wav, 0] = np.exp(np.log(nhi) + fac * np.log(nlo / nhi))
                         if klo <= 0 or khi < 0:
                             ref_index[wav, 1] = 0
                         else:
-                            ref_index[wav, 1] = np.exp(np.log(khi) + fac * np.log(klo/khi))
+                            ref_index[wav, 1] = np.exp(np.log(khi) + fac * np.log(klo / khi))
                     else:
                         # non-conducting interpolation, linear decreasing k, constant n
                         ref_index[wav, 0] = float(data[-1].split()[1])
                         ref_index[wav, 1] = float(data[-1].split()[2]) * \
-                                            float(data[-1].split()[0])/wave
+                                            float(data[-1].split()[0]) / wave
 
             # stop searching the files
             break
@@ -312,7 +312,7 @@ def _cloud_nk_data(species, wavelength):
             'The following cloud particle bulk species is currently ' +
             'not supported or misspelled: ' + species + '. Vacuum values ' +
             'are used instead'
-            )
+        )
 
     # return refractive indices
     return ref_index
@@ -325,10 +325,10 @@ def patch_cloud_mix_opa(self, clouds=None):
     if clouds is not None:
         lsk = self.line_struc_kappas
         self.line_struc_kappas = np.zeros((lsk.shape[0], lsk.shape[1],
-                                           lsk.shape[2]+1, lsk.shape[3]))
+                                           lsk.shape[2] + 1, lsk.shape[3]))
         self.line_struc_kappas[:, :, :-1, :] = lsk
-self.line_struc_kappas[:, :, -1, :] = np.ones_like(self.line_struc_kappas[:, :, -1, :])*clouds[0][np.newaxis,:,:]
-        self.continuum_opa_scat_emis += clouds[1]
+        self.line_struc_kappas[:, :, -1, :] = np.ones_like(self.line_struc_kappas[:, :, -1, :]) \
+                                              * clouds[0][np.newaxis, :, :]
 
 
 def patch_delete_clouds(self, clouds=None):
@@ -339,14 +339,14 @@ def patch_delete_clouds(self, clouds=None):
         self.line_struc_kappas = self.line_struc_kappas[:, :, :-1, :]
 
 
-def patch_calc_transm(self,temp,abunds,gravity,mmw,P0_bar,R_pl,
-                sigma_lnorm = None,
-                fsed = None, Kzz = None, radius = None,
-                Pcloud = None,
-                kappa_zero = None,
-                gamma_scat = None,
-                contribution = False, haze_factor = None,
-                gray_opacity = None, variable_gravity=True, clouds=None):
+def patch_calc_transm(self, temp, abunds, gravity, mmw, P0_bar, R_pl,
+                      sigma_lnorm=None,
+                      fsed=None, Kzz=None, radius=None,
+                      Pcloud=None,
+                      kappa_zero=None,
+                      gamma_scat=None,
+                      contribution=False, haze_factor=None,
+                      gray_opacity=None, variable_gravity=True, clouds=None):
     """
     This function is used to update petitRADTRANS to include clouds.
     """
